1739eeb59b26e99352f09336ec6eaaafb0870f38
Matheus Caetano <caetanomatheus876@gmail.com>
2022-05-22 21:22:32 -0300

    Created basic API logic

---
+++ backend/app/Http/Controllers/OrphanageController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Orphanage;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class OrphanageController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return response()->json(Orphanage::all());
+    }
+
+    public function show(int $id): JsonResponse
+    {
+        return response()->json(Orphanage::find($id));
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        return response()->json(Orphanage::create($request->all()));
+    }
+
+    public function update(int $id, Request $request): JsonResponse
+    {
+        return response()->json(
+            Orphanage::whereKey($id)->update($request->all())
+        );
+    }
+
+    public function destroy(int $id, ): JsonResponse
+    {
+        return response()->json(Orphanage::whereKey($id)->delete());
+    }
+}
